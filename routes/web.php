97139c7e8382261eaec1dbff2455a75bd84da55a
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-08-04 20:26:00 +0700

    third

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin/dashboard',[
-        "tittle"=>"dashboard"
+    return view('login',[
+        "tittle"=>"login-admin"
     ]);
 });
 Route::get('/login-admin', function () {
-    return view('login',[
-        "tittle"=>"login-admin"
+    return view('admin/dashboard',[
+        "tittle"=>"dashboard"
     ]);
 });
 
